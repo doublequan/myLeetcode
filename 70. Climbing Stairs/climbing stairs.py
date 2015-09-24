26d53639de4ad92ff04279ecfc9ba8b238b756fd
doublequan <msa_quanquan@163.com>
2015-09-24 15:53:08 +0800

    update python solution of 70

---
+++ 70. Climbing Stairs/climbing stairs.py
@@ -0,0 +1,18 @@
+__author__ = 'Bill Quan'
+import math
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        for j in range(0, n/2 + 1):
+            i = n - 2 * j
+            count += math.factorial(i+j) / (math.factorial(i) * math.factorial(j))
+        return count
+
+print Solution.climbStairs(Solution(), 5)
+
+
